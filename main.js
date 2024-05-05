d9c7134802748180f5d07351791fffa923819062
Faisal Muhiuddin <136606230+faisalmuhiuddin@users.noreply.github.com>
2024-05-06 01:04:42 +0530

    Added query parameters

--- main.js
+++ main.js
@@ -12,11 +12,28 @@ app.get('/api/data', (req, res) => {
             return;
         }
 
-        const jsonData = JSON.parse(data);
-        res.json(jsonData);
+        let jsonData = JSON.parse(data);
+        const { type, week, day, meal } = req.query;
+
+         if (type) {
+            jsonData = jsonData[type];
+            if (week) {
+                jsonData = jsonData?.[week];
+                if (day) {
+                    jsonData = jsonData?.[day];
+                    if (meal) {
+                        jsonData = jsonData?.[meal];
+                    }
+                }
+            }
+        }
+
+        res.json(jsonData || {});
     });
 });
 
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 })
+
+// http://localhost:3000/api/data?type=South%20Veg&week=odd&day=Sunday&meal=Breakfast
